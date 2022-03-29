c3652080130ab331b739cc6b9643ba3dcc19535c
AvilaMarco <marco6267@hotmail.com>
2022-03-29 20:49:04 -0300

    Fix: events on map in all devices refactor: class CSS convert to SASS styles

--- src/main/resources/static/web/js/main.js
+++ src/main/resources/static/web/js/main.js
@@ -50,10 +50,36 @@ ubicacionesMap.forEach((area) => {
   areahtml.id = id;
   areahtml.shape = "circle";
   areahtml.alt = "aremap";
-  areahtml.coords = `${x}, ${y}, ${r}`;
+  areahtml.coords = `${resizeCoord(x, "width")}, ${resizeCoord(y, "height")}, ${r}`;
   areahtml.addEventListener("click", selectGame);
   document.querySelector("map[name*=mapeo]").appendChild(areahtml);
 });
+
+
+function isMobile(){
+  return visualViewport.width < 1024 
+  // ToDo: try more test about determinated if a device be or not mobile
+  // navigator.userAgentData.mobile
+}
+
+function resizeCoord(coord, side){  
+  if(isMobile()) return coord
+  const currentSize = visualViewport[side] * 0.75
+  const referentSize = side === "width" ? 1400 : 800
+  const conversionFactor =  currentSize / referentSize;
+  return coord * conversionFactor;
+}
+
+function resizeCoordWithMargin(coords){
+  const [ x, y ] = coords.split(",")
+  if(isMobile()) return [ x, y ]
+
+  const marginSizeX = visualViewport.width * 0.125
+  const marginSizeY = visualViewport.height * 0.125
+  const coordX = parseFloat(x) + marginSizeX
+  const coordY = parseFloat(y) + marginSizeY
+  return [ coordX, coordY ]
+}
 
 function randomNation(){
   const nations = ["FIRE", "EARTH", "AIR", "WATER"]
@@ -253,17 +279,28 @@ function crearJuegosMap(games) {
   );
   games.forEach((e) => {
     if (e.game_players[0].Score == null) {
-      let areahtml = document.querySelector("#" + e.direction);
-      let div = document.createElement("div");
+      const { id, coords, dataset } = document.querySelector("#" + e.direction);
+      const [ width, height ] = resizeCoordWithMargin(coords)
+      const div = document.createElement("div");
+      div.id = id;
       div.classList.add("selectMap");
+      div.style.left  = width + "px";
+      div.style.top   = height + "px";
       div.dataset.game = "true";
+      div.dataset.gameid = e.id;
+      div.dataset.location = dataset.location;
+
+      div.addEventListener("click", selectGame);
       if (e.game_players.length == 2) {
+
         div.dataset.gpid1 = e.game_players[0].id;
         div.dataset.playerid1 = e.game_players[0].player.id;
+        div.dataset.playername1 = e.game_players[0].player.email;
+
         div.dataset.gpid2 = e.game_players[1].id;
         div.dataset.playerid2 = e.game_players[1].player.id;
-        div.dataset.playername1 = e.game_players[0].player.email;
         div.dataset.playername2 = e.game_players[1].player.email;
+
         if (e.game_players.some((f) => f.player.id == playerData.id)) {
           div.dataset.name = "Enter";
           div.classList.add("SelectEnter");
@@ -272,9 +309,11 @@ function crearJuegosMap(games) {
           div.dataset.name = "InGame";
         }
       } else {
+
         div.dataset.gpid1 = e.game_players[0].id;
-        div.dataset.playername1 = e.game_players[0].player.email;
         div.dataset.playerid1 = e.game_players[0].player.id;
+        div.dataset.playername1 = e.game_players[0].player.email;
+        
         if (e.game_players.some((f) => f.player.id == playerData.id)) {
           div.dataset.name = "Enter";
           div.classList.add("SelectEnter");
@@ -283,20 +322,7 @@ function crearJuegosMap(games) {
           div.dataset.name = "Join";
         }
       }
-      div.addEventListener("click", selectGame);
-      div.dataset.gameid = e.id;
-      if (recargarMapa && screen.width < 1024) {
-        div.style.top = parseInt(areahtml.coords.split(",")[1]) + 113 + "px";
-        div.style.left = parseInt(areahtml.coords.split(",")[0]) + 109 + "px";
-      } else if (screen.width < 1024) {
-        div.style.top = parseInt(areahtml.coords.split(",")[1]) - 37 + "px";
-        div.style.left = parseInt(areahtml.coords.split(",")[0]) - 41 + "px";
-      } else {
-        div.style.top = parseInt(areahtml.coords.split(",")[1]) + 47 + "px";
-        div.style.left = parseInt(areahtml.coords.split(",")[0]) + 166 + "px";
-      }
-      div.dataset.location = areahtml.dataset.location;
-      div.id = areahtml.id;
+
       document.querySelector("#pivotMap").appendChild(div);
     }
   });
@@ -308,15 +334,20 @@ function selectGame(event) {
       .querySelector("#pivotMap")
       .removeChild(document.querySelector("div[data-name*='selectGame']"));
   }
-  const { id, dataset, coords, style } = event.target;
+
+  const { id, dataset, coords } = event.target;
+  const [ width, height ] = resizeCoordWithMargin(coords)
   let div = document.createElement("div");
-  div.dataset.name = "selectGame";
+  div.dataset.id = id;
   div.classList.add("selectMap");
+  div.style.left  = width + "px";
+  div.style.top   = height + "px";
+  div.dataset.name = "selectGame";
+  div.dataset.location = dataset.location;
   div.addEventListener("click", removeSelect);
+
   if (dataset.game == "true") {
     div.classList.add("selectGameCreate");
-    div.style.top = style.top;
-    div.style.left = style.left;
     document.querySelectorAll("div [name*='dataGame']").forEach((e) => {
       e.classList.remove("hidden");
       if (!(e.innerText == "Info") && !(dataset.playerid1 == playerData.id)) {
@@ -330,10 +361,6 @@ function selectGame(event) {
     });
   } else {
     div.classList.add("selectCreate");
-    const width = screen.width * (screen.width > 1024 ? 0.107 : 0.09);
-    const height = screen.height * (screen.width > 1024 ? 0.09 : 0.05);
-    div.style.top = parseInt(coords.split(",")[1]) + height + "px";
-    div.style.left = parseInt(coords.split(",")[0]) + width + "px";
 
     document.querySelectorAll("div [name*='dataGame']").forEach((e) => {
       e.classList.remove("hidden");
@@ -343,8 +370,7 @@ function selectGame(event) {
     });
   }
 
-  div.dataset.location = dataset.location;
-  div.dataset.id = id;
+  
   document.querySelector("#pivotMap").appendChild(div);
   if (
     document.querySelector(
@@ -465,6 +491,7 @@ function viewMapa(event) {
   let back = document.querySelector("#back");
   back.classList.remove("hidden");
   back.addEventListener("click", viewMenu);
+
   let reload = document.querySelector("#reload");
   reload.classList.remove("hidden");
   reload.addEventListener("click", function () {
@@ -475,25 +502,12 @@ function viewMapa(event) {
       document.querySelector("#reload").classList.remove("Animation-reload");
     }, 1100);
   });
+
   document.querySelector("#mapabg").classList.add("hidden");
   document
     .querySelectorAll("button[name*='botonesMenu']")
     .forEach((e) => e.classList.add("hidden"));
-  if (
-    document.querySelector("div[data-game*='true']") != null &&
-    !document.querySelector("div[data-game*='true']").dataset.move
-  ) {
-    document.querySelectorAll("div[data-game*='true']").forEach((e) => {
-      if (screen.width < 1024) {
-        e.style.top = parseInt(e.style.top.split("px")[0]) + 150 + "px";
-        e.style.left = parseInt(e.style.left.split("px")[0]) + 150 + "px";
-      } else {
-        e.style.top = parseInt(e.style.top.split("px")[0]) + "px";
-        e.style.left = parseInt(e.style.left.split("px")[0]) + "px";
-      }
-      e.dataset.move = true;
-    });
-  }
+
 }
 
 //LOGOUT
@@ -577,263 +591,3 @@ function saveUserData() {
 function goGame() {
   location.assign("/web/game.html");
 }
-
-
-
-// function selectGameCreate(event) {
-//     if (document.querySelector("div[data-name*='selectGame']")!=null) {
-//         document.querySelector("#pivotMap").removeChild(document.querySelector("div[data-name*='selectGame']"))
-//     }
-//     let click = event.target
-//     document.querySelectorAll("div[data-name*='game']").forEach(e=>e.classList.remove("selectGame"))
-//     if (click.name == "texto" || click.name == "texto2"){
-//         click.parentNode.classList.add("selectGame")
-//     }else{
-//         click.classList.add("selectGame")
-//     }
-//     console.log(event.target)
-// }
-// let datosDelJuego = document.querySelector("div#"+document.querySelector("div[data-name*='selectGame']").dataset.id)
-// console.log(datosDelJuego)
-// console.log(event.innerText)
-
-// let datos = document.querySelector("div[data-name*='selectGame']")
-// let datosjoingame = document.querySelector("div.selectGame")
-// let isPlayerCreator = false
-// let game;
-// if (null != datosjoingame) {
-//     game = gamesData.filter(e=>e.id==datosjoingame.dataset.gameid)[0]
-//    isPlayerCreator = gamesData.filter(e=>e.id==datosjoingame.dataset.gameid)[0].gameplayers.some(e=>e.player.id==playerData.id)
-// }else{
-//     isPlayerCreator = false
-// }
-// if (datos!=null && datosjoingame==null && !isPlayerCreator) {
-//     // crear juegos
-//     crearjuego(datos.dataset.location,datos.id)
-// }else if(datos==null && datosjoingame!=null && !isPlayerCreator){
-//     // unirse a juego
-//     joinGame(parseInt(datosjoingame.dataset.gameid))
-// }else if(isPlayerCreator && datos==null && datosjoingame!=null){
-//     // volver al juego
-//     console.log(parseInt(datosjoingame.dataset.gameid))
-//     let gp = game.gameplayers.filter(e=>e.player.id == playerData.id?e.id:null)
-//     if (gp!=null){
-//         console.log(gp)
-//         location.href = "/web/game.html?gp="+gp[0].id
-//         // location.assign("/web/game.html?gp="+gp[0].id);
-//     }else{
-//         // modo espectador proximamente
-//         alert("no perteneces al juego")
-//     }
-// }
-// else{
-//     console.log("elegir una ubicacion en el mapa")
-// }
-// let divselect = document.querySelector("div[data-name*='selectGame']")
-// let y,x = null
-// if (divselect!=null){
-//     y = parseInt(divselect.style.top.split("px")[0])
-//     x = parseInt(divselect.style.left.split("px")[0])
-// }
-// let marginMap = document.querySelector("#mapa").style
-// if (event.target.innerText == "Map") {
-//     event.target.innerText = "Menu"
-//     document.querySelectorAll("button[name*='botonesMenu']").forEach(e=>e.classList.add("hidden"))
-//     document.querySelector("#mapabg").classList.add("hidden")
-//     if (divselect!=null) {
-//         document.querySelector("#mapa").style.marginLeft = "0"
-//         document.querySelector("#mapa").style.marginTop = "0"
-//         if (masx) {
-//             divselect.style.left = x - 150 + "px"
-//         }else if(masy){
-//             divselect.style.top = y - 150 + "px"
-//         }
-//     }
-// }else{
-//    event.target.innerText = "Map"
-//     document.querySelector("#mapabg").classList.remove("hidden")
-//     document.querySelectorAll("button[name*='botonesMenu']").forEach(e=>e.classList.remove("hidden"))
-//     if (divselect!=null) {
-//         if (y < 100) {
-//             divselect.style.top = y + 150 + "px"
-//             marginMap.marginTop = "150px"
-//             masy = true
-//         }else if (x < 100) {
-//             divselect.style.left = x + 150 + "px"
-//             marginMap.marginLeft = "150px"
-//             masx = true
-//         }
-//     }
-// }
-//runweb
-// function runweb(json){
-// document.querySelector("#Player").classList.add("iconTransparent"+json.player.nacion)
-// crear si existen juegos en el mapa
-// mapaGames()
-// createTableRanking()
-// createTableGames();
-// }
-// function enterGame(event){
-//     fetch('/api/games',{
-//     method: 'GET',
-//     }).then(function(response){if(response.ok){return response.json()}
-//     }).then(function(JSON){
-//         if (JSON.player != "guest") {
-//             let game = JSON.games.filter(e=>e.id == event.target.dataset.gameid);
-//             if (event.target.dataset.players.includes((""+JSON.player.id))) {
-//                 let gp = game[0].gameplayers.filter(e=>e.player.id == JSON.player.id)
-//                 location.assign("/web/game.html?gp="+gp[0].id);
-//                 console.log(gp[0].id)
-//             }else if (game[0].gameplayers.length == 1) {
-//                 joinGame(game[0].id)
-//             }else{
-//                 alert("you can't join the game")
-//             }
-//         }else{
-//             alert("Login to join or create a game")
-//         }
-//     });
-// }
-//TABLE GAMES
-// function createTableGames(){
-//     let player;
-//     fetch('/api/games',{
-//     method: 'GET',
-//     }).then(function(response){if(response.ok){return response.json()}
-//     }).then(function(json){
-//     games = json.games;
-//     player = json.player
-//     tablegame.innerHTML = "";
-//     let tabla = "";
-//     for(let i = 0;i<games.length;i++){
-//         tabla +=`<tr>
-//         <td>
-//             <p>${games[i].id}</p>
-//         </td>
-//         <td>
-//             <p>${games[i].created}</p>
-//         </td>`
-//         if (games[i].gameplayers[0].Score != null) {
-//             tabla +=`<td><p>Finish</p></td>`
-//         }else{
-//             tabla +=`<td><p>in Game</p></td>`
-//         }
-//         if (player != "guest") {
-//             if (games[i].gameplayers.map(e=>e.player.id).includes(player.id) && games[i].gameplayers[0].Score == null) {
-//                tabla +=`
-//                 <td>
-//                     <button id="entergame" data-players="${games[i].gameplayers.map(e=>e.player.id)}" data-gameid="${games[i].id}">enter game</button>
-//                 </td>
-//                 <td>
-//                     <p>${(games[i].gameplayers.length==2?
-//                         (games[i].gameplayers[1].player.id == player.id?games[i].gameplayers[0].player.id:games[i].gameplayers[1].player.id) : "non-rival")}</p>
-//                 </td>`
-//             }else if(games[i].gameplayers.length==1){
-//                 tabla +=`
-//                 <td>
-//                    <p>${games[i].gameplayers[0].player.id}</p>
-//                 </td>
-//                 <td>
-//                     <button id="joingame" data-players="${games[i].gameplayers.map(e=>e.player.id)}" data-gameid="${games[i].id}">join game</button>
-//                 </td>`
-//             }else{
-//                 tabla +=`
-//                 <td>
-//                    <p>${games[i].gameplayers[0].player.id}</p>
-//                 </td>
-//                 <td>
-//                     <p>${games[i].gameplayers[1].player.id}</p>
-//                 </td>`
-//             }
-//         }else{
-//             tabla +=`
-//                 <td>
-//                    <p>${games[i].gameplayers[0].player.id}</p>
-//                 </td>
-//                 <td>
-//                     <p>${(games[i].gameplayers.length==2?games[i].gameplayers[1].player.id : "non-rival")}</p>
-//                 </td>`
-//         }
-
-//     }
-// tablegame.innerHTML = tabla
-//     document.querySelectorAll('#entergame').forEach(e=>e.addEventListener('click',enterGame))
-//     document.querySelectorAll('#joingame').forEach(e=>e.addEventListener('click',joinGame))
-//     });
-// }
-
-// function createGame(){
-//     fetch('/api/games',{
-//         method:'POST'
-//     })
-//     .then(function(response){
-//         if(response.ok){
-//             createTableGames()
-//             return response.json()
-//         }else{
-//             throw new Error(response.json());
-//         }
-//     })
-//     .then(function(JSON){
-//         console.log("entrar al juego")
-//         console.log(JSON.gpid)
-//         location.assign("/web/game.html?gp="+JSON.gpid);
-//     })
-//     .catch(error => error)
-//     .then(json => console.log(json))
-// // }
-
-// function verDatosUser() {
-//   let modalDiv = document.querySelector(".div-modal");
-//   modalDiv.innerHTML = "";
-//   modalDiv.classList.remove("bg-" + playerData.nation);
-//   modalDiv.classList.add("bg-" + playerData.nation);
-
-//   randomNation()
-//   let div = document.createElement("div");
-//   div.classList.add("datosUser");
-//   div.classList.add("bg-color-" + playerData.nation);
-//   div.style.color = "white";
-//   const side = playerData.nation == "agua" || playerData.nation == "tierra" ? "left" : "right"
-//   div.classList.add("datosUser-"+side);
-
-//   let userName = document.createElement("P");
-//   userName.innerText = playerData.email;
-//   userName.classList.add("text-userName");
-
-//   let divText = document.createElement("div");
-//   divText.classList.add("divText");
-
-//   let nation = document.createElement("P");
-//   nation.innerText = playerData.nation + "natin";
-
-//   let name = document.createElement("P");
-//   name.innerText = playerData.name;
-//   divText.appendChild(name);
-//   divText.appendChild(nation);
-
-//   let table = document.createElement("TABLE");
-//   console.log(playerData.id);
-//   console.log(tableUser(playerData.id));
-//   table.classList.add("table-user");
-//   table.innerHTML += tableUser(playerData.id);
-//   div.appendChild(userName);
-//   div.appendChild(divText);
-//   div.appendChild(table);
-//   modalDiv.appendChild(div);
-
-//   const userDataHTML = `
-//     <div class="datosUser datosUser-${side} bg-color-${playerData.nation}">
-//       <p class="text-userName">${playerData.email}</p>
-//       <div class="divText">
-//         <p>${playerData.name}</p>
-//         <p>${playerData.nation}</p>
-//       </div>
-//       <table class="table-user">
-//         ${tableUser(playerData.id)}
-//       </table>
-//     </div>
-//   `
-
-//   modalDiv.innerHTML = userDataHTML;
-// }
